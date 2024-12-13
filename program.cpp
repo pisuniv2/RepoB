b5f444cc9bc743f929bb6cf19ab4d009ac65b2ad
Nikita Kholchev <nikitabel66@mail.ru>
2024-12-13 15:01:44 +0500

    Добавлен код для вывода разности чисел

---
+++ program.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b;
+    cout << "Enter A and B: ";
+    cin >> a >> b;
+    cout << "A + B = " << a + b << '\n';
+    cout << "A - B = " << a - b << '\n';  // Добавлена разность чисел
+    return 0;
+}
